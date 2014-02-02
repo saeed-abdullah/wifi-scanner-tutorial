d93e1744ea7af4ff4b1ee6f3db8b421bf048f69b
Saeed Abdullah <sma249@cornell.edu>
2014-02-02 17:39:30 -0500

    Wifi Scanner tutorial.

---
+++ Wifi/src/main/java/edu/cornell/cs/pac/ubicomp/MainActivity.java
@@ -0,0 +1,112 @@
+package edu.cornell.cs.pac.ubicomp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    private WifiManager wifiManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+        setUpWifi();
+    }
+
+    private void setUpWifi() {
+        wifiManager = (WifiManager)this.getSystemService(Context.WIFI_SERVICE);
+        IntentFilter intentFilter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        this.registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                handleWifiScanResult();
+            }
+        }, intentFilter);
+    }
+    public void startScan(View view) {
+        if(!wifiManager.isWifiEnabled()) {
+            Toast.makeText(this, "Enabling Wifi", Toast.LENGTH_LONG);
+            wifiManager.setWifiEnabled(true);
+        }
+
+        wifiManager.startScan();
+    }
+
+    private void handleWifiScanResult() {
+        // Get location label
+
+        String location = ((EditText)findViewById(R.id.edit_label)).getText().toString();
+        StringBuffer sb = new StringBuffer();
+        for(ScanResult e: wifiManager.getScanResults()) {
+            sb.append(e.BSSID);
+            sb.append(": ");
+            sb.append(e.SSID);
+            sb.append("\n");
+        }
+
+        TextView textView = (TextView) findViewById(R.id.text_scan_result);
+        textView.setText(sb.toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+
+}
